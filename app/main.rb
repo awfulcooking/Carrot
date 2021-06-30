32923b519b4e9cdd11122fe017e336bc4a4ad407
mooff <mooff@together.beer>
2021-06-30 20:59:06 +0100

    Work in progress

--- app/main.rb
+++ app/main.rb
@@ -1,6 +1,17 @@
-def tick args
-  args.outputs.labels  << [640, 500, 'Hello World!', 5, 1]
-  args.outputs.labels  << [640, 460, 'Go to docs/docs.html and read it!', 5, 1]
-  args.outputs.labels  << [640, 420, 'Join the Discord! http://discord.dragonruby.org', 5, 1]
-  args.outputs.sprites << [576, 280, 128, 101, 'dragonruby.png']
+require 'smaug.rb'
+require 'lib/optcarrot-combined.rb'
+require 'app/requires.rb'
+
+Maw!
+
+class Fixnum
+  def [](index)
+    to_s(2)[index].to_i
+  end
 end
+
+def tick args
+  solids << [0,0,1280,720,255,0,0]
+  # puts "Hello world"
+end
+puts "Hello world"
